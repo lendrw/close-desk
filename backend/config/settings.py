19fad135a3d712eee154c4bdbdb0ad3c02a88860
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2026-06-24 13:25:59 -0300

    chore: configure JWT authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -86,6 +86,9 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "accounts.User"
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
